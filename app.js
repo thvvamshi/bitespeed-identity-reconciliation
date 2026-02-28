881f233df0d59e8aee3967e009598e7d1734d614
thvvamshi <bodavamshikumar2002@gmail.com>
2026-02-28 15:40:07 +0530

    chore: added the health check route

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const identityRoutes = require("./routes/identityRoutes");
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
+
+// Health check endpoint
+app.get("/", (req, res) => {
+  res.send("Bitespeed Identity Reconciliation Service is running.");
+});
+
+// Use identity routes
 app.use("/", identityRoutes);
 
 module.exports = app;
